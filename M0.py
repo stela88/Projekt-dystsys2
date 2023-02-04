67886c5b4967423fef041a996fdc0b4677bf2318
stela88 <49338331+stela88@users.noreply.github.com>
2023-02-04 12:01:40 +0100

    update sa ClientSession

--- M0.py
+++ M0.py
@@ -1,27 +1,32 @@
 import asyncio
 import aiofiles
 import json
+import aiohttp
 import pandas as pd
 from aiohttp import web
 
 routes = web.RouteTableDef()
 
 
+async def process_data(client, code):
+    avg_letters = sum(len(word) for word in code.split()) / len(code.split())
+    print(f"For {client}, average number of letters is: {avg_letters}")
+
 @routes.get("/JsonData")
 async def json_data(request):
-    async with aiofiles.open('file-000000000040.json', mode='r') as file_data:
-        read_data = {await file_data.readline() for _ in range(10)}
-        whole_data = [json.loads(line) for line in read_data]
-        client_ids = [f"client{i}" for i in range(10)]
-        database = {}
-        for i, item in enumerate(whole_data):
-            db_item = {"python_code": item["content"]}
-            database[client_ids[i]] = db_item
-        # print(database)
-        for client in database:
-            code = database[client]['python_code']
-            avg_letters = sum(len(word) for word in code.split()) / len(code.split())
-            print(f"For {client}, average number of letters is: {avg_letters}")
+    async with aiohttp.ClientSession() as session:
+        async with aiofiles.open('file-000000000040.json', mode='r') as file_data:
+            read_data = [await file_data.readline() for _ in range(10)]
+            whole_data = [json.loads(line) for line in read_data]
+            client_ids = [f"client{i}" for i in range(10)]
+            database = {}
+            tasks = []
+            for i, item in enumerate(whole_data):
+                db_item = {"python_code": item["content"]}
+                database[client_ids[i]] = db_item
+                task = asyncio.create_task(process_data(client_ids[i], db_item["python_code"]))
+                tasks.append(task)
+            await asyncio.gather(*tasks)
         return web.json_response(database, status=200)
 
 
