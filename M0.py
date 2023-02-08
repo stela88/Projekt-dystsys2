e7f6c6c3bb6c7c2fed4fee8ef9216975270e7425
stela88 <49338331+stela88@users.noreply.github.com>
2023-02-08 07:46:02 +0100

    proba

--- M0.py
+++ M0.py
@@ -1,35 +1,48 @@
 import asyncio
-import aiofiles
-import json
 import aiohttp
 import pandas as pd
-from aiohttp import web
-
-routes = web.RouteTableDef()
 
 
-async def process_data(client, code):
-    avg_letters = sum(len(word) for word in code.split()) / len(code.split())
-    print(f"For {client}, average number of letters is: {avg_letters}")
+async def process_code(clients):
+    tasks = []
+    results = []
 
-@routes.get("/JsonData")
-async def json_data(request):
     async with aiohttp.ClientSession() as session:
-        async with aiofiles.open('file-000000000040.json', mode='r') as file_data:
-            read_data = [await file_data.readline() for _ in range(10)]
-            whole_data = [json.loads(line) for line in read_data]
-            client_ids = [f"client{i}" for i in range(10)]
-            database = {}
-            tasks = []
-            for i, item in enumerate(whole_data):
-                db_item = {"python_code": item["content"]}
-                database[client_ids[i]] = db_item
-                task = asyncio.create_task(process_data(client_ids[i], db_item["python_code"]))
-                tasks.append(task)
-            await asyncio.gather(*tasks)
-        return web.json_response(database, status=200)
+        for id, codes in clients.items():
+            tasks.append(
+                asyncio.create_task(session.get("http://127.0.0.1:8080/", json={"client": id, "codes": codes})))
+        results = await asyncio.gather(*tasks)
+        results = [await x.json() for x in results]
+    return results
 
 
-app = web.Application()
-app.router.add_routes(routes)
-web.run_app(app, port=8081)
+async def main():
+    print("Running client script...\n")
+
+    # generate list of client IDs
+    list_of_client_ids = list(range(1, 10001))
+
+    # load dataframe
+    print("Loading dataframe...\n")
+    dataframe = pd.read_json("data/dataset.json", lines=True)
+    print("Dataframe loaded.\n")
+
+    # calculate rows per client
+    rows_per_client = int(len(dataframe) / len(list_of_client_ids))  # 10
+
+    # create dict for client IDs and their code
+    clients = {id: [] for id in list_of_client_ids}
+    for id, codes in clients.items():
+        from_row = (id - 1) * rows_per_client
+        to_row = from_row + rows_per_client
+        for index, row in dataframe.iloc[from_row + 1:to_row + 1].iterrows():
+            codes.append(row.get("content"))
+
+    results = await process_code(clients)
+
+    # log average number of letters for clients' code
+    for result in results:
+        print("Average code length for client with ID", result.get("client"), "is", result.get("averageWordcount"))
+
+
+asyncio.get_event_loop().run_until_complete(main())
---
+++ proba_klijenta.py
@@ -0,0 +1,43 @@
+import asyncio
+import aiofiles
+import json
+import aiohttp
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+async def process_data(client, code):
+    avg_letters = sum(len(word) for word in code.split()) / len(code.split())
+    print(f"For {client}, average number of letters is: {avg_letters}")
+
+@routes.get("/JsonData")
+async def json_data(request):
+    async with aiohttp.ClientSession() as session:
+        async with aiofiles.open('file-000000000040.json', mode='r') as file_data:
+            read_data = [await file_data.readline() for _ in range(10)]
+            whole_data = [json.loads(line) for line in read_data]
+            client_ids = [f"client{i}" for i in range(10)]
+            tasks = []
+            database = []
+            for i, item in enumerate(whole_data):
+                db_item = {"client_id": client_ids[i], "python_code": item["content"]}
+                database.append(db_item)
+                baza = database
+                tasks.append(
+                    asyncio.create_task(session.post("http://1.0.0.127:8082/JsonData", json=database)))
+                final_results = await asyncio.gather(*baza)
+                final_results = [await x.json() for x in final_results]
+
+
+            return web.json_response(final_results, status=200)
+
+"""""
+            async with session.post("http://0.0.0.0:8082/JsonData", json=database) as res:
+                result = await res.json()
+                """
+
+
+
+app = web.Application()
+app.router.add_routes(routes)
+web.run_app(app, port=8080)
